96e3ba7fe1151ff0fc5bfc4f039aa38efdd14fc7
elloambientaleng-hue <elloambiental.eng@gmail.com>
2026-05-17 15:57:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@
    automaticamente — ninguém precisa limpar cache na mão.
    ============================================================ */
 
-const CACHE_VERSION = 'zello-v22';   // <<< INCREMENTE A CADA DEPLOY
+const CACHE_VERSION = 'zello-v25';   // <<< INCREMENTE A CADA DEPLOY
 const CACHE_NAME = CACHE_VERSION;
 
 // Arquivos que ficam em cache para funcionar offline
