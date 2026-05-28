41c9f38b78f49c032724384b87c9c1df83a75bd8
elloambientaleng-hue <elloambiental.eng@gmail.com>
2026-05-27 22:19:29 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@
    sem o SW atrapalhar.
    ============================================================ */
 
-const CACHE_VERSION = 'zello-v185';
+const CACHE_VERSION = 'zello-v186';
 const CACHE_NAME = CACHE_VERSION;
 
 const ARQUIVOS_ESSENCIAIS = [
